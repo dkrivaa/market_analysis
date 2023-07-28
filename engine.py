c58d5898d7070def596c40476c17abbae6b94c0a
dkrivaa <dannykrivaa@hotmail.com>
2023-07-28 11:09:06 +0300

    Nasdaq stock market analysis

--- engine.py
+++ engine.py
@@ -128,3 +128,5 @@ def low_5():
 
 def ticker():
     df = get_data()
+    st.markdown(f'<span style="color: #18448c; font-size: 22px"><b>Choose Ticker/Company</b></span>'
+                , unsafe_allow_html=True)
--- pages/Ticker.py
+++ pages/Ticker.py
@@ -5,6 +5,5 @@ import engine
 
 engine.page_header()
 
-st.markdown(f'<span style="color: #18448c; font-size: 22px"><b>Choose Ticker/Company</b></span>'
-            , unsafe_allow_html=True)
+
 
