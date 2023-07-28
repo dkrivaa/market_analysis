900ba204b09ab17a489ea71df98f37dc2f198968
dkrivaa <dannykrivaa@hotmail.com>
2023-07-28 09:59:07 +0300

    Nasdaq stock market analysis

--- engine.py
+++ engine.py
@@ -86,7 +86,7 @@ def big_10():
     df = get_data()
     big_10 = df.nlargest(10, 'marketCap')
     big_10['change'] = big_10['change']/100
-    big_10['color'] = big_10['change'].apply(lambda x: 'red' if x < 0 else 'green')
+    big_10['color'] = big_10['change'].apply(lambda x: '#DB5343' if x < 0 else '#7FDB43')
     c = alt.Chart(big_10).mark_bar().encode(
         x=alt.X('change', axis=alt.Axis(format='%')),
         y='company name',
