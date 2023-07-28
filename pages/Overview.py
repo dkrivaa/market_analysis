8a6612d619b5e2295a5b35abc9c836db73030b8a
dkrivaa <dannykrivaa@hotmail.com>
2023-07-28 09:30:23 +0300

    Nasdaq stock market analysis

--- pages/Overview.py
+++ pages/Overview.py
@@ -8,7 +8,11 @@ engine.page_header()
 with st.container():
     cols = st.columns(2)
     with cols[0]:
+        st.markdown(f'<span style="color: #18448c; font-size: 14px"><b>Top 5</b></span>'
+                    , unsafe_allow_html=True)
         engine.top_5()
     with cols[1]:
+        st.markdown(f'<span style="color: #18448c; font-size: 14px"><b>Bottom 5</b></span>'
+                    , unsafe_allow_html=True)
         engine.low_5()
 
