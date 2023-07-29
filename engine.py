8861d3a2909f0bd7a1fb11105df6c35ba507e9c4
dkrivaa <dannykrivaa@hotmail.com>
2023-07-29 08:27:00 +0300

    Nasdaq stock market analysis

--- engine.py
+++ engine.py
@@ -144,6 +144,8 @@ def ticker():
         with cols[0]:
             ticker = st.selectbox('Choose Ticker', options=df['symbol'],
                                   key='ticker')
+            name = df.loc[df['symbol'] == st.session_state.ticker, ['company name']]
+
 
         with cols[1]:
             name = st.selectbox('Choose Company', options=df['company name'],
